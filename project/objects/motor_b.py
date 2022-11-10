a0271b3e11f6a4f3c2e1e1437751b19b98a6a7df
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2022-11-09 19:14:15 -0500

    motor-b new algorithm

--- project/objects/motor_b.py
+++ project/objects/motor_b.py
@@ -8,8 +8,18 @@ class Motor_B:
     def __init__(self):
         #Assign port of motor
         self.motor = Motor("B")
+        #Assign default position of the robot
+        self.position = 0
     
-    #Rotate the motor given a command (command can be = to 0,1,2,3,4)
+    #Update the robot's position
+    def set_position(self,new_position):
+        self.position = new_position
+
+    #Rotate the motor given a command (command can be = to 0,1,2,3,4) and based on the robots initial y-position
     def push(self,command):
-        print("Moving to row", command)
-        #@Todo : implement the correct movements given the command
+        print("Moving to row", command,"from ypos",self.position)
+        #@Todo : implement the correct movements given the command and based on the robots initial y-position
+        
+        #Update the position of the robot based on command given
+        self.set_position(command)
+        
