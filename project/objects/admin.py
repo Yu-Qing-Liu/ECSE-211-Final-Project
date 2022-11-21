c190953fddb1f606f8c3500cc1c3874924506ef7
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2022-11-21 10:26:14 -0500

    Added error codes to main

--- project/objects/admin.py
+++ project/objects/admin.py
@@ -1,54 +1,38 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import * 
-from utils.grid_elem import Gridelem
+#!/usr/bin/python3
+from utils.brick import TouchSensor, wait_ready_sensors
+import time
 
 class Admin:
     #Constructor
     def __init__(self):
-        self.inputs = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.inputs = []
 
-    #Modify inputs
-    def set_inputs(self,new_inputs):
-        self.inputs = new_inputs
-    
-    #Get inputs
-    def get_inputs(self):
-        return self.inputs
-
-    #Initialize the gui
+    #Starts the query process using touch sensors
     def start(self):
 
-        root = Tk()
+        TOUCH_SENSOR_0 = TouchSensor(1)
+        TOUCH_SENSOR_1 = TouchSensor(2)
+        wait_ready_sensors(True)
 
-        # This is the section of code which creates the main window
-        root.geometry('350x500')
-        root.configure(background='#F0F8FF')
-        root.title('Admin')
+        arr=[]
+        arr = [0 for i in range(25)]
+        index = 0
 
+        while True:
+            time.sleep(0.2)
+            if TOUCH_SENSOR_1.is_pressed():
+                print("1")
+                arr[index] = 1
+                index += 1
+                time.sleep(0.1)
+            if TOUCH_SENSOR_0.is_pressed():
+                print("0")
+                index += 1
+                time.sleep(0.1)
+            if index == 25:
+                break
 
-        # This is the section of code which creates the button grid
-        
-        y_pos = 50
-        button_id = 0
-        for i in range(5):
-            for j in range(5):
-                grid_button = Gridelem(button_id,root,self.get_inputs())
-                self.inputs = grid_button.get_inputs()
-                grid_button.button.place(x=50+50*j,y=y_pos)
-                button_id += 1
-            y_pos += 50
+        self.inputs = arr
 
-        #Start button onClick function
-
-        def on_click():
-            root.destroy()
-            return None
-
-        Button(root, text='Start Drawing', bg="#FFFFFF", font=('arial', 12, 'normal'), command=on_click).place(x=50,y=350)
-
-        root.mainloop()
-    
-    #Reset the inputs
-    def reset(self):
-        self.inputs = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    def get_inputs(self):
+        return self.inputs
